d1a04a4fe7bba935794ba48330a341578e36c129
Jigar Pael <30416256+drone911@users.noreply.github.com>
2020-04-19 19:26:28 +0530

    added cors

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use(
 );
 app.use(express.static(path.join(__dirname,'static')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/", Routes);
 
 app.listen(app.get('port'), () => {
